c1b474653641e6ca195b345425934f1f72df36e3
kulaginDV <kulagin1104@yandex.ru>
2023-06-21 22:52:51 +0300

    decision task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 10:
+Написать программу, которая принимает на вход 
+трехзначное число и на выходе показывает вторую цифру этого числа
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+Console.WriteLine ("Input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void SecFig (int a)
+{
+    int num2 = a / 10 % 10;
+    Console.Write ("Second figure in your number - " + num2);
+}
+
+if (num < 100 || num >= 1000)
+{
+    Console.Write ("You input incorrect number!");
+}
+else
+{
+    SecFig (num);
+}
